225002fd87e4f63bf4063ca9b6e4137e8345500b
Justin McAfee <justinamcafee+github@gmail.com>
2018-04-13 17:45:55 -0500

    Added Admin User checking
    Added admin user verification to insure that file copies don't fail due to permissions. Code taken from Microsoft's [Hey Scripting Guy](https://blogs.technet.microsoft.com/heyscriptingguy/2011/05/11/check-for-admin-credentials-in-a-powershell-script/).

--- AutoTron.ps1
+++ AutoTron.ps1
@@ -1,4 +1,7 @@
-﻿#Setting Variables
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`[Security.Principal.WindowsBuiltInRole] "Administrator")){
+        Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+        Break}
+#Setting Variables
 $file="tron.exe"
 echo "Setting location to $env:userprofile\Downloads\" 
 Set-Location $env:userprofile\Downloads\
