9840b12d767d92f91b5dc0e4028ca62698c38b9b
Justin McAfee <justinamcafee+github@gmail.com>
2018-04-13 17:51:52 -0500

    Corrected Admin checking function
    Bad bracketing/code style.

--- AutoTron.ps1
+++ AutoTron.ps1
@@ -1,6 +1,9 @@
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`[Security.Principal.WindowsBuiltInRole] "Administrator")){
-        Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
-        Break}
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+[Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+Break
+}
 #Setting Variables
 $file="tron.exe"
 echo "Setting location to $env:userprofile\Downloads\" 
